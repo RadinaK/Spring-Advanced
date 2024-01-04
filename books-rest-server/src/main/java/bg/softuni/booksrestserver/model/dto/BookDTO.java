9b2308d1e72b0ac630cabf59bffd0e04d9c8ffbc
Radina Krasteva <radinakrsteva@gmail.com>
2024-01-04 16:49:58 +0200

    start project

---
+++ books-rest-server/src/main/java/bg/softuni/booksrestserver/model/dto/BookDTO.java
@@ -0,0 +1,54 @@
+package bg.softuni.booksrestserver.model.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class BookDTO {
+    private Long id;
+    private String title;
+
+    @NotEmpty
+    @Size(min = 5)
+    private String isbn;
+
+    private AuthorDTO author;
+
+    public BookDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public BookDTO setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public BookDTO setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public BookDTO setIsbn(String isbn) {
+        this.isbn = isbn;
+        return this;
+    }
+
+    public AuthorDTO getAuthor() {
+        return author;
+    }
+
+    public BookDTO setAuthor(AuthorDTO author) {
+        this.author = author;
+        return this;
+    }
+}
